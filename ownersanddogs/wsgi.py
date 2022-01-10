d8f72893c0eaa6befea8a97d9873170d239eb866
shawn365 <eogkr.kim@gmail.com>
2022-01-10 13:02:33 +0900

    create: ownersanddogs

---
+++ ownersanddogs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ownersanddogs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ownersanddogs.settings')
+
+application = get_wsgi_application()
