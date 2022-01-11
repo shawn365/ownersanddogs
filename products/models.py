75311fd8c83b87966893ba86cf13751a8ee7203f
shawn365 <eogkr.kim@gmail.com>
2022-01-11 11:38:46 +0900

    create: actor, movie models and post, get views

--- products/models.py
+++ products/models.py
@@ -12,7 +12,30 @@ class Dog(models.Model):
     name = models.CharField(max_length=45)
     age = models.CharField(max_length=5)
     
-    owner_id = models.ForeignKey('Owner', on_delete=models.CASCADE)
+    owner = models.ForeignKey('Owner', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'dogs'
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=300)
+    date_of_birth = models.DateField()
+    class Meta:
+        db_table = 'actors'
+
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    release_date = models.DateField()
+    
+    running_time = models.TimeField()
+
+    class Meta:
+        db_table = 'movies'
+
+class Actor_Movie(models.Model):
+    actor = models.ForeignKey('Actor', on_delete=models.CASCADE)
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'actors_movies'
--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ import json
 from django.http import JsonResponse
 from django.views import View
 
-from products.models import Owner, Dog
+from products.models import Owner, Dog, Actor_Movie, Actor, Movie
 
 class ProductsView(View):
     def post(self, request):
@@ -21,7 +21,7 @@ class ProductsView(View):
         dog = Dog.objects.create(
             name     = data['dogname'],
             age      = data['dogage'],
-            owner_id = owner
+            owner_id = owner.id
         )
         return JsonResponse({'messasge':'created'}, status=201)
 
@@ -38,14 +38,58 @@ class ProductsView(View):
                 {
                     "dog_name" : dog.name,
                     "dog_age" : dog.age,
-                    "owner_name" : dog.owner_id.name,
-                    "owner_email" : dog.owner_id.email,
-                    "owner_age" : dog.owner_id.age
+                    "owner_name" : dog.owner.name,
+                    "owner_email" : dog.owner.email,
+                    "owner_age" : dog.owner.age
                 }
             )
 
         print(f"results objects :: {results}")
 
         return JsonResponse({"dogs and owners" :results}, status=200) 
+
+    def post(self, request):
+        data     = json.loads(request.body)
+
+        actor     = Actor.objects.create(
+            first_name    = data['firstname'],
+            last_name     = data['lastname'],
+            date_of_birth = data['birthday']
+        )
+        movie = Movie.objects.create(
+            title        = data['title'],
+            release_date = data['releasedate'],
+            running_time = data['runningtime']
+        )
+        actor_movie = Actor_Movie.objects.create(
+            actor_id = actor.id,
+            movie_id = movie.id        
+        )
+        return JsonResponse({'messasge':'created'}, status=201)
+
+
+    def get(self, request):
+        #GET 127.0.0.1:8000/product
+        actors_movies = Actor_Movie.objects.all()
+        print(f"actors_movies objects :: {actors_movies}")
+
+        results = []
+
+        for actor_movie in actors_movies:
+            results.append(
+                {
+                    "first_name" : actor_movie.actor.first_name,
+                    "last_name" : actor_movie.actor.last_name,
+                    "date_of_birth" : actor_movie.actor.date_of_birth,
+                    "title" : actor_movie.movie.title,
+                    "release_date" : actor_movie.movie.release_date,
+                    "running_time" : actor_movie.movie.running_time
+                }
+            )
+
+        print(f"results objects :: {results}")
+
+        return JsonResponse({"actors and movies" :results}, status=200) 
+
 
 
